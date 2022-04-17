ac033a4ddf3555e8883d8edb868f78871c4cecb0
Nikolay Putko <reg@omni-k.ru>
2022-04-17 22:24:56 +0300

    Добавлены служебные файлы

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='resistance_calculator',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Nikolay Putko',
+    author_email='nikolay@putko.ru',
+    description=''
+)
