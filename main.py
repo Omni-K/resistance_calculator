7dfab79d4fd47de7b0e25782efb08d8cff925e8a
king_of_kings <reg@omni-k.ru>
2022-04-17 10:18:33 +0300

    migrate to kivyMD

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
+from kivymd.app import MDApp
+from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDRaisedButton
+from kivy.properties import ColorProperty
 
-
-nominal_colors = [(0, 0, 0, 1), # black
-                  (0.3, 0.3, 0, 1),  # brown
-                  (1, 0, 0, 1),  # red
-                  (1, 0.8, 0, 1),  # orange
-                  (1, 1, 0, 1),  # yellow
-                  (0, 1, 0, 1),  # green
-                  (0, 0.8, 1, 1),  # blue
-                  (1, 0.3, 1, 1),  # purple
-                  (0.1, 0.1, 0.2, 1),  # grey
-                  (1, 1, 1, 1),  # white
+nominal_colors = [[0, 0, 0, 1],  # black
+                  [0.3, 0.3, 0, 1],  # brown
+                  [1, 0, 0, 1],  # red
+                  [1, 0.7, 0, 1],  # orange
+                  [1, 1, 0, 1],  # yellow
+                  [0, 1, 0, 1],  # green
+                  [0, 0.8, 1, 1],  # blue
+                  [1, 0.3, 1, 1],  # purple
+                  [0.1, 0.1, 0.2, 1],  # grey
+                  [1, 1, 1, 1],  # white
                   ]
 
-class MainApp(App):
-    def build(self):
-        layout = BoxLayout(orientation='vertical')
-        layout.add_widget(Button(text='placeholder'))
-        grid = GridLayout(cols=5, rows=15)
 
+class MainApp(MDApp):
+    def build(self):
+        layout = MDBoxLayout(orientation='vertical')
+        layout.add_widget(MDRectangleFlatButton(text='placeholder'))
+        grid = MDGridLayout(cols=5, rows=15)
 
         # button.bind(on_press=self.on_press_button)
         for rown in range(10):
             for coln in range(5):
-                btn = Button(text=str(rown),
-                             size_hint=(0.5, 0.5),
-                             pos_hint={'center_x': .5, 'center_y': .5},
-                             background_color=nominal_colors[rown]
-                             )
+                btn = MDRaisedButton(text=str(rown),
+                                     size_hint=(0.5, 0.1),
+                                     pos_hint={'center_x': .5, 'center_y': .5},
+                                     md_bg_color=nominal_colors[rown],
+                                     )
                 grid.add_widget(btn)
         layout.add_widget(grid)
         return layout
@@ -42,4 +42,3 @@ class MainApp(App):
 if __name__ == '__main__':
     app = MainApp()
     app.run()
-
